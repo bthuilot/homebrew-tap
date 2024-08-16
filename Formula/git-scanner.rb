ddb6093bc3eab8602f09292eff13ba2bee76e1a6
Bryce Thuilot <bryce@thuilot.io>
2024-08-16 11:27:42 -0400

    fix: typo
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- Formula/git-scanner.rb
+++ Formula/git-scanner.rb
@@ -3,7 +3,7 @@ class GitScanner < Formula
   homepage "https://github.com/bthuilot/git-scanner"
   version "0.1.0"
   license "GPL-3.0-or-later"
-  url "https://github.com/gitleaks/git-scanner/archive/refs/tags/v#{version}.tar.gz"
+  url "https://github.com/bthuilot/git-scanner/archive/refs/tags/v#{version}.tar.gz"
   sha256 "fea36ca9a2220c66268530dbb97d4913824106802f8a1acaf04137895c4229f3"
   head "https://github.com/bthuilot/git-scanner.git", branch: "main"
 
