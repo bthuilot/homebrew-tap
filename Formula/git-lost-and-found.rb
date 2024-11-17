6d5a53d94fb6e757c3d8c4041e96f08e06fcbce8
Bryce Thuilot <bryce@thuilot.io>
2024-11-17 17:34:24 -0500

    fix(git-lost-and-found): use v1.0.1
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- Formula/git-lost-and-found.rb
+++ Formula/git-lost-and-found.rb
@@ -1,10 +1,10 @@
 class GitLostAndFound < Formula
   desc "Script to find dangling commits in git repositories"
   homepage "https://github.com/bthuilot/git-lost-and-found"
-  version "1.0.0"
+  version "1.0.1"
   license "GPL-3.0-or-later"
   url "https://github.com/bthuilot/git-scanner/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "8de736eb987a9547b8d2eafb394d0b78c9958337060ff3d5e30dcf78d8a7d6a2"
+  sha256 "88f4ec6508e38057c5675d856209a2c26a2d963824fbb12a7ebc6bd5744b326d"
   head "https://github.com/bthuilot/git-lost-and-found.git", branch: "main"
 
   depends_on "go" => :build
