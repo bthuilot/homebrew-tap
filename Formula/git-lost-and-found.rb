b46b5e4517ac0ab82d9a7e17e180945ba4f092ae
Bryce Thuilot <bryce@thuilot.io>
2025-02-09 12:54:08 -0500

    chore(git-lost-and-found): update to v2.0.1 (#4)
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

--- Formula/git-lost-and-found.rb
+++ Formula/git-lost-and-found.rb
@@ -1,19 +1,21 @@
 class GitLostAndFound < Formula
   desc "Script to find dangling commits in git repositories"
   homepage "https://github.com/bthuilot/git-lost-and-found"
-  version "1.0.1"
+  version "2.0.1"
   license "GPL-3.0-or-later"
   url "https://github.com/bthuilot/git-scanner/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "88f4ec6508e38057c5675d856209a2c26a2d963824fbb12a7ebc6bd5744b326d"
+  sha256 "09c4410903bc531226ae868df78a4ec2f3d3910ff8ef59515e17cbc3062e203c"
   head "https://github.com/bthuilot/git-lost-and-found.git", branch: "main"
 
   depends_on "go" => :build
+  depends_on "make" => :build
   depends_on "gitleaks" => :recommended
   depends_on "trufflehog" => :recommended
   
 
   def install
-    system "go", "build", *std_go_args(ldflags:"-s -w")
+    system "make", "build"
+    bin.install "./bin/git-lost-and-found" => "git-lost-and-found"
 
     generate_completions_from_executable(bin/"git-lost-and-found", "completion")
   end
