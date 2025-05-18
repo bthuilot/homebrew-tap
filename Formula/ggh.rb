17f88e36d7a34b8bd003901b49835a3dfda4377d
Bryce Thuilot <bryce@thuilot.io>
2025-05-18 13:20:36 -0400

    feat(ggh): add GGH formula
    Signed-off-by: Bryce Thuilot <bryce@thuilot.io>

---
+++ Formula/ggh.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+ALL_HOOKS = %w[
+  pre-commit
+  commit-msg
+  applypatch-msg
+  post-update
+  pre-merge-commit
+  pre-receive
+  update
+  pre-applypatch
+  pre-push
+  prepare-commit-msg
+  fsmonitor-watchman
+  pre-rebase
+  push-to-checkout
+  post-commit
+].freeze
+
+class Ggh < Formula
+  VERSION = '0.1.2'
+
+  desc 'Personalized, global git hooks'
+  homepage 'https://github.com/bthuilot/ggh'
+  version VERSION
+  license 'GPL-3.0-or-later'
+  url "https://github.com/bthuilot/ggh/archive/refs/tags/v#{VERSION}.tar.gz"
+  sha256 'a83f79c4c4068efe35e1b1dcc73156182f8aaf021a41f0cfc03d54145b80070b'
+
+  depends_on 'opam' => :build
+  depends_on 'git'
+
+  # TODO(byce): support bottles
+  # bottle do
+  #   root_url "https://github.com/bthuilot/ggh/releases/download/v#{VERSION}/ggh-v#{VERSION}-darwin-arm64.tar.gz"
+  #   sha256 cellar: :any_skip_relocation, arm64_sequoia: ""
+  #   sha256 cellar: :any_skip_relocation, arm64_sonoma: ""
+  #   sha256 cellar: :any_skip_relocation, arm64_ventura: ""
+  #   sha256 cellar: :any_skip_relocation, arm64_monterey: ""
+  #   sha256 cellar: :any_skip_relocation, big_sur: ""
+  # end
+
+  def install
+    opamroot = buildpath / '.opam'
+
+    ENV['OPAMROOT'] = opamroot
+    ENV['OPAMYES'] = '1'
+
+    system 'opam', 'init', '--no-setup', '--disable-sandboxing'
+    system 'opam', 'update'
+    system 'opam', 'exec', '--', 'opam', 'install', '.', '--deps-only', '-y'
+    system 'opam', 'exec', '--', 'dune', 'build'
+
+    bin.install buildpath / '_build/default/bin/ggh/main.exe' => 'ggh'
+
+    ALL_HOOKS.each do |hook|
+      bin.install_symlink bin / 'ggh' => "hooks/#{hook}"
+    end
+  end
+
+  def post_install
+    system 'opam', 'exec', '--', 'dune', 'clean'
+  end
+
+  def caveats
+    hooks_dir = bin / 'hooks'
+    <<~CAVEATS
+      WARNING: ggh is not currently configured,
+      please run `GGH_HOOKS_DIR=#{hooks_dir} ggh configure` to set ggh as your global hooks
+    CAVEATS
+  end
+
+  test do
+    system "#{bin}/ggh --help"
+  end
+end
--- Formula/git-lost-and-found.rb
+++ Formula/git-lost-and-found.rb
@@ -1,25 +1,26 @@
-class GitLostAndFound < Formula
-  desc "Script to find dangling commits in git repositories"
-  homepage "https://github.com/bthuilot/git-lost-and-found"
-  version "2.0.1"
-  license "GPL-3.0-or-later"
-  url "https://github.com/bthuilot/git-scanner/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "09c4410903bc531226ae868df78a4ec2f3d3910ff8ef59515e17cbc3062e203c"
-  head "https://github.com/bthuilot/git-lost-and-found.git", branch: "main"
+# frozen_string_literal: true
 
-  depends_on "go" => :build
-  depends_on "make" => :build
-  depends_on "gitleaks" => :recommended
-  depends_on "trufflehog" => :recommended
-  
+class GitLostAndFound < Formula
+  desc 'Script to find dangling commits in git repositories'
+  homepage 'https://github.com/bthuilot/git-lost-and-found'
+  version '2.0.1'
+  license 'GPL-3.0-or-later'
+  url "https://github.com/bthuilot/git-scanner/archive/refs/tags/v#{version}.tar.gz"
+  sha256 '09c4410903bc531226ae868df78a4ec2f3d3910ff8ef59515e17cbc3062e203c'
+  head 'https://github.com/bthuilot/git-lost-and-found.git', branch: 'main'
+
+  depends_on 'go' => :build
+  depends_on 'make' => :build
+  depends_on 'gitleaks' => :recommended
+  depends_on 'trufflehog' => :recommended
 
   def install
-    system "make", "build"
-    bin.install "./bin/git-lost-and-found" => "git-lost-and-found"
+    system 'make', 'build'
+    bin.install './bin/git-lost-and-found' => 'git-lost-and-found'
 
-    generate_completions_from_executable(bin/"git-lost-and-found", "completion")
+    generate_completions_from_executable(bin / 'git-lost-and-found', 'completion')
   end
-  
+
   test do
     system "#{bin}/git-lost-and-found --help"
   end
